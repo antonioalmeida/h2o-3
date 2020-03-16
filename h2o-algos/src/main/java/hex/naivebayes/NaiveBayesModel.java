42268f3eed266c9d36acf1cb8e93bc89faf07b20
zuzanaolajcova <zuzana.olajcova@h2o.ai>
2020-03-16 14:54:02 +0100

    PUBDEV-7237_move_effective_params_computation_to_computeEffectiveParameters()

--- h2o-algos/src/main/java/hex/naivebayes/NaiveBayesModel.java
+++ h2o-algos/src/main/java/hex/naivebayes/NaiveBayesModel.java
@@ -55,10 +55,9 @@ public class NaiveBayesModel extends Model<NaiveBayesModel,NaiveBayesModel.Naive
 
   public NaiveBayesModel(Key selfKey, NaiveBayesParameters parms, NaiveBayesOutput output) { 
     super(selfKey,parms,output);
-    initEffectiveParam();
   }
   
-  void initEffectiveParam() {
+  public void initEffectiveParam() {
     EffectiveParametersUtils.initFoldAssignment(_parms, _effective_parms);
     _effective_parms._stopping_metric = null;
     _effective_parms._distribution = null;
